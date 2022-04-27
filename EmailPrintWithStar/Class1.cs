2ed7a9fd86c2e47b2feee405812e657449d50dd6
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-04-27 13:07:42 +0530

    Add project files.

---
+++ EmailPrintWithStar/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailPrintWithStar
+{
+    internal class Class1
+    {
+        public static void emailprint()
+        {
+            Console.WriteLine("Enter your Email:");
+            string email= Console.ReadLine();
+
+            string name= email.Substring(0,email.IndexOf("@"));
+
+            //string abc= email.Substring(email.IndexOf("@"),email.Length);
+
+            char[] charArr = name.ToCharArray();
+
+            for (int i = 0; i <charArr.Length; i++)
+            {
+                if (i<=2)
+                    Console.Write(charArr[i]);
+                else
+                    Console.Write("*");
+            }
+            Console.WriteLine("@gmail.com");
+            Console.ReadLine();
+        }
+    }
+}
